cb7f276cd40a11e6e733a298ef939fc5731127db
lxingy3 <145194392+lxingy3@users.noreply.github.com>
2026-05-30 17:11:26 +0800

    Restore site footer

--- app/layout.js
+++ app/layout.js
@@ -1,4 +1,5 @@
 import './globals.css';
+import { SiteFooter } from './components/SiteFooter';
 
 export const metadata = {
   title: 'AlgoHub',
@@ -8,7 +9,10 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className="flex min-h-screen flex-col">
+        {children}
+        <SiteFooter />
+      </body>
     </html>
   );
 }
