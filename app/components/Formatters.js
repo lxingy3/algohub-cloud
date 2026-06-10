a766a27e983317c688d7bd1b4f008977dd02a3a4
Xiangyu Li <3367250374@qq.com>
2026-06-10 17:42:01 +0800

    Fix home event dates

--- app/components/Formatters.js
+++ app/components/Formatters.js
@@ -4,10 +4,11 @@ export function formatStatus(value) {
 
 export function formatDate(value) {
   if (!value) return '';
+  const date = value instanceof Date ? value : new Date(value);
+  if (Number.isNaN(date.getTime())) return '';
   return new Intl.DateTimeFormat('en-US', {
     month: 'short',
     day: 'numeric',
     year: 'numeric',
-  }).format(value);
+  }).format(date);
 }
-
